1765cdc1f4fa6ab19b3a310afc4c98f3df23f17c
Jonas Schenke <jonas@groeger-clan.de>
2019-01-28 07:16:59 +0100

    Added timer.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
@@ -77,20 +78,33 @@ int main(int argc, char *argv[]) {
   std::cout << "maximal chunk size: " << max_chunk_size << "\n";
   std::cout << "minimal chunk size: " << min_chunk_size << "\n";
   std::cout << "entry count: " << entry_count << "\n";
-
+  
   // stream file
   std::function<void(Entry *, std::size_t, std::size_t)> sort =
       [](Entry *entries, std::size_t chunk_start, std::size_t chunk_size) {
         quicksort<Entry>(&entries[chunk_start], chunk_size);
       };
   FileStreamer<Entry> fs(entry_count, min_chunk_size, max_chunk_size);
+  
+  auto start = std::chrono::high_resolution_clock::now();
+
   fs.read(input_path, sort);
 
+  auto middle = std::chrono::high_resolution_clock::now();
+  
   // write file
   std::function<void(Entry *, std::size_t)> write = fs.write(output_path);
   merge_lists<Entry>(write, entry_count, fs.get(), max_chunk_size,
                      fs.getChunkStarts());
   fs.flush_write();
+  
+  auto end = std::chrono::high_resolution_clock::now();
+
+  auto first_part = std::chrono::duration_cast<std::chrono::milliseconds>(middle - start);
+  auto second_part = std::chrono::duration_cast<std::chrono::milliseconds>(end - middle);
+  auto complete = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+  std::cout << "Timing: " << first_part.count() << "\t+ " << second_part.count() << "\t= " << complete.count() << "\n";
 
   return EXIT_SUCCESS;
 }
